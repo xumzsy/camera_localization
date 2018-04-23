6e537dd9c9b83645ac2abe88cc24f2787693d2b3
Mengze Xu <mxu29@jhu.edu>
2018-04-22 20:41:05 -0400

    add more functions, remain to implement updating feature map stratry and optimization to solve camera pose

--- src/localization_node.cc
+++ src/localization_node.cc
@@ -1,18 +1,28 @@
 
-//#include <cv_bridge/cv_bridge.h>
+#include <opencv2/xfeatures2d.h>
 
 #include "localization_node.h"
 
 namespace CameraLocalization {
     
 CameraLocalizationNode::CameraLocalizationNode(int max_feature_num)
-    : max_feature_num_(max_feature_num){
+    : max_feature_num_(max_feature_num), initialized_(false){
+        
+    GetParameters();
     raw_image_subscriber_ = node_handle_.subscribe<sensor_msgs::Image>
-        ("/usb_cam/image_raw", 10, &CameraLocalizationNode::RawImageCallback, this);
+        ("/usb_cam/image_raw", 10, &CameraLocalizationNode::ImageCallback, this);
     camera_pose_publisher_ = node_handle_.advertise<geometry_msgs::PoseStamped>("/pose", 10);
+        feature_detector_ = xfeatures2d::SURF::create();
 }
     
-void CameraLocalizationNode::RawImageCallback(const ::sensor_msgs::Image::ConstPtr& raw_image){
+void CameraLocalizationNode::GetParameters(){
+    if(node_handle_.getParam("",cx)){
+        std::cout<<"Fail to get parameter"<<std::endl;
+    }
+}
+    
+    
+void CameraLocalizationNode::ImageCallback(const ::sensor_msgs::Image::ConstPtr& raw_image){
     // convert ros image to opencv image cv_ptr->image
     cv_bridge::CvImageConstPtr cv_ptr;
     try{
@@ -23,26 +33,85 @@ void CameraLocalizationNode::RawImageCallback(const ::sensor_msgs::Image::ConstP
         return;
     }
     
-    // Calculate the pose
-    CalculatePose(cv_ptr);
+    // Address the image
+    AddressImage(cv_ptr);
 }
 
-void CameraLocalizationNode::CalculatePose(cv_bridge::CvImageConstPtr cv_ptr){
-    // Calculate key points and descriptor
-    /*
-    cv::Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create();
-    detector->setHessianThreshold(minHessian);
+void CameraLocalizationNode::AddressImage(cv_bridge::CvImageConstPtr cv_ptr){
+    // Calculate key points
+    std::vector<cv::KeyPoint> keypoints;
+    cv::Mat feature_descriptors;
+    feature_detector_->detect(cv_ptr->image, keypoints);
     
-    std::vector<KeyPoint> keypoints_1, keypoints_2;
-    Mat descriptors_1, descriptors_2;
-    detector->detectAndCompute( img_1, Mat(), keypoints_1, descriptors_1 );
-    detector->detectAndCompute( img_2, Mat(), keypoints_2, descriptors_2 );
-    */
-    std::cout<<(cv_ptr==nullptr)<<std::endl;
+    // sort keypoints by response
+    std::sort(keypoints.begin(), keypoints.end(),[](const cv::KeyPoint& a, const cv::KeyPoint& b){
+        return a.response > b.response;});
+    
+    // compute feature descriptors
+    feature_detector_->compute(cv_ptr->image, keypoints, feature_descriptors);
+    
+    geometry_msgs::PoseStamped camera_pose;
+    camera_pose.header = cv_ptr->header;
+    // Initialize if not
+    if(!initialized_){
+        // identity position
+        camera_pose.pose.position.x = 0.0;
+        camera_pose.pose.position.y = 0.0;
+        camera_pose.pose.position.z = 0.0;
+        camera_pose.pose.orientation.x = 0.0;
+        camera_pose.pose.orientation.y = 0.0;
+        camera_pose.pose.orientation.z = 0.0;
+        camera_pose.pose.orientation.w = 1.0;
+        initialized_ = true;
+    } else{
+        // match and calculate camera pose
+        cv::FlannBasedMatcher descriptor_matcher_;
+        std::vector<cv::DMatch> matches;
+        descriptor_matcher_.match(feature_descriptors, feature_map_.descriptors, matches);
+        
+        // filter the matches
+        double min_match_distance = DBL_MAX;
+        for(auto match:matches) min_match_distance = min(min_match_distance, match.distance);
+        const double match_distance_threshold = 2 * min_match_distance;
+        
+        // optimize reprojection error to solve pose
+        // for good_match, need some math and codes for optimization
+        // Rememmber to update matched times
+    }
+    
+    // Update trajectory and publish the pose
+    camera_trajectory_.push_back(camera_pose);
+    camera_pose_publisher_.publish(camera_pose);
+    
+    // Updateing existing Feature Map History
+    for(auto time:feature_map_.history_time) time++;
+    
+    // Update Feature Map
+    const int camera_pose_index = camera_trajectory_.size() - 1;
+    if(feature_map_.points.size()+feature_descriptors.size()<=max_feature_num){
+        for(int i=0;i<keypoints.size();++i){
+            feature_map_.points.push_back(keypoints[i]);
+            feature_map_.descriptors.push_back(feature_descriptors[i]);
+            feature_map_.history_time.push_back(0);
+            feature_map_.matched_times.push_back(0);
+            feature_map_.image_index.push_back(camera_pose_index);
+        }
+    } else{
+        // score = score(history, matched_time)
+        vector<int> scores;
+        for(int i=0;i<keypoints.size();++i){
+            scores.push_back(feature_map_.matched_time[i]*100000-history_time);
+        }
+        
+        
+    }
+    
 }
 
 void CameraLocalizationNode::UpdateFeatureMap(){
-    
+    // Mainly maintain the keypoints reprojects into the first image
+    // correspond descriptors and some history image
+    // which image it belongs (mainly we need the position related to first image)
 }
 
     
--- src/localization_node.h
+++ src/localization_node.h
@@ -4,7 +4,6 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv/cv.h>
 
-//#include "opencv.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "sensor_msgs/Image.h"
 
@@ -17,37 +16,52 @@ public:
     CameraLocalizationNode(int max_feature_num);
     
 private:
-    struct FeatureMap{
-        ::std::vector</*Point*/int> points;
-        ::std::vector</*Descriptor*/int> descriptors;
-        ::std::vector<int> history_time;
-        ::std::vector<int> matched_times;
+    struct Parameters{
+        
     };
     
+    
+    struct FeatureMap{
+        std::vector<cv::KeyPoint> points;
+        cv::Mat descriptors;
+        std::vector<int> history_time;
+        std::vector<int> matched_times;
+        std::vector<int> image_index;
+    };
+    
+    // Get Parameters
+    void GetParameters();
+    
     // Receive a image, transform it into opencv img and address it
-    void RawImageCallback(const sensor_msgs::Image::ConstPtr& raw_image);
+    void ImageCallback(const sensor_msgs::Image::ConstPtr& raw_image);
     
     // Receive a list of key points and descriptors, calculate camera pose
     // with the help of opencv
-    void CalculatePose(cv_bridge::CvImageConstPtr cv_ptr);
+    void AddressImage(cv_bridge::CvImageConstPtr cv_ptr);
     
     // Update the feature map
     void UpdateFeatureMap();
     
-    // Publish camera pose
-    void PublishCameraPoseCallback();
+    ros::NodeHandle node_handle_;
+    ros::Subscriber raw_image_subscriber_;
+    ros::Publisher camera_pose_publisher_;
+    cv::Ptr<Feature2D> feature_detector_;
     
-    ::ros::NodeHandle node_handle_;
-    ::ros::Subscriber raw_image_subscriber_;
-    ::ros::Publisher camera_pose_publisher_;
+    // camera info (parameters)
+    double cx;
+    double cy;
+    double fx;
+    double fy;
+    
     
     const int max_feature_num_;
+    bool initialized_;
     
     // feature map ? considering matching, how to maintain it
     FeatureMap feature_map_;
     
     // trajectory
-    ::std::vector<geometry_msgs::PoseStamped> camera_trajectory_;
+    std::vector<geometry_msgs::PoseStamped> camera_trajectory_;
 };
 
 } // namespace CameraLocalizationNode
